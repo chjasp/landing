55e63b8475c7c1f3cf32ad5c5e7deee7c80dd5a6
chjasp <ch.jasper1@gmail.com>
2021-05-05 11:40:09 +0200

    new

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -38,8 +38,8 @@ function LandingPage() {
         <form className="contact-form" onSubmit={sendEmail}>
           <StyledCol>
             <h5>Beta registration</h5>
-            <input type="text" name="user_name" placeholder="Name" />
-            <input className="mail" type="email" name="user_email" placeholder="E-Mail" />
+            <BigInput type="text" name="user_name" placeholder="Name" />
+            <BigInput className="mail" type="email" name="user_email" placeholder="E-Mail" />
             <StyledCheck>
               <input type="checkbox" name="checkbox" />
               <span>Apply for the paid Feedback-Session</span>
@@ -54,16 +54,25 @@ function LandingPage() {
 
 export default LandingPage;
 
+const BigInput = styled.input`
+  height: 5vh;
+  width: 30vh;
+  max-width: 300px;
+  font-size: 1.3rem;
+
+  @media(max-width: 460px) {
+    font-size: 1rem;
+  }
+`;
+
 const StyledInp = styled.input`
   color: white !important;
   text-align: center;
   background-color: transparent;
   border: 2px solid white;
-
-  @media (max-width: 960px) {
-    margin-left: 0px;
-    margin-top: 0px;
-  }
+  height: 5vh;
+  font-size: 1.3rem;
+  width: 10vh;
 
   &:hover {
     background-color: white;
@@ -71,6 +80,9 @@ const StyledInp = styled.input`
     -webkit-transition: background-color 0.3s;
     -webkit-transition: color 0.3s;
   }
+
+  @media(max-width: 560px) {
+    font-size: 1rem;
 `;
 
 const StyledCheck = styled.div`
@@ -84,7 +96,25 @@ const StyledCheck = styled.div`
 
   span {
     margin-left: 1rem;
+    font-size: 1.3rem;
   }
+
+  input {
+    width:2.5vh;
+    height:2.5vh;
+  }
+
+  @media(max-width: 560px) {
+    span {
+      color: white;
+      margin-left: 1rem;
+      font-size: 1rem;
+    }
+
+    input {
+      width:2vh;
+      height:2vh;
+    }
 `;
 
 const StyledBye = styled.div`
@@ -92,17 +122,28 @@ const StyledBye = styled.div`
 
   h5 {
     color: white;
-    font-size: 1rem;
+    font-size: 2rem;
 
     &.bottom {
       margin-top: 2rem;
     }
   }
+
+  @media(max-width: 560px) {
+    h5 {
+      color: white;
+      font-size: 1rem;
+  
+      &.bottom {
+        margin-top: 2rem;
+      }
+    }
+  }
 `;
 
 const StyledCol = styled(Col)`
-  // margin-top: -20vh;
   margin: auto;
+  min-height: 50vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -119,5 +160,13 @@ const StyledCol = styled(Col)`
   h5 {
     color: white;
     margin-bottom: 1.5rem;
-    font-size: 1.5rem;
+    font-size: 2rem;
+  }
+
+  @media(max-width: 560px) {
+    h5 {
+      color: white;
+      margin-bottom: 1.5rem;
+      font-size: 1.3rem;
+  }
 `;
